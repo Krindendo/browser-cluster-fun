3681f258a61c8e24272248bcf30545dd60cb262a
Krindendo work <lollipopfunkyx9@gmail.com>
2023-12-02 23:29:20 +0100

    copied code

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,141 @@
-import Image from 'next/image'
+import { PathMe } from "@/lib/path";
+import { SVGProps, useEffect, useRef, useState } from "react";
+
+type WindowDetails = {
+  screenX: number;
+  screenY: number;
+  screenWidth: number;
+  screenHeight: number;
+  width: number;
+  height: number;
+  updated: number;
+};
 
 export default function Home() {
+  const [timers, setTimers] = useState<ReturnType<typeof setInterval>[]>([]);
+  const svgRef = useRef<SVGProps<SVGSVGElement>>();
+  const pathRef = useRef<SVGProps<SVGPathElement>>();
+
+  function getScreens(): [string, WindowDetails][] {
+    return Object.entries(window.localStorage)
+      .filter(([key]) => key.startsWith("screen-"))
+      .map(([key, value]: [string, string]) => [
+        key,
+        JSON.parse(value) as WindowDetails,
+      ]);
+  }
+
+  function getScreenId() {
+    const existingScreens = Object.keys(window.localStorage)
+      .filter((key) => key.startsWith("screen-"))
+      .map((key) => parseInt(key.replace("screen-", "")))
+      .sort((a, b) => a - b);
+
+    return existingScreens.at(-1) + 1 || 1;
+  }
+  const screenId = `screen-${getScreenId()}`;
+
+  function setScreenDetails() {
+    const windowDetails = {
+      screenX: window.screenX,
+      screenY: window.screenY,
+      screenWidth: window.screen.availWidth,
+      screenHeight: window.screen.availHeight,
+      width: window.outerWidth,
+      height: window.innerHeight,
+      updated: Date.now(),
+    };
+    window.localStorage.setItem(screenId, JSON.stringify(windowDetails));
+    // console.log(windowDetails);
+  }
+
+  function displayStats() {
+    if (!stats) return;
+    const existingScreens = Object.fromEntries(getScreens());
+    stats.innerHTML = JSON.stringify(existingScreens, null, " ");
+  }
+
+  function restart() {
+    console.log(timers);
+    timers.forEach((timer) => window.clearInterval(timer));
+    window.localStorage.clear();
+    setTimeout(() => window.location.reload(), Math.random() * 1000);
+  }
+
+  function removeScreen() {
+    console.log(`removing screen ${screenId}`);
+    localStorage.removeItem(screenId);
+  }
+
+  function removeOld() {
+    const screens = getScreens();
+    for (const [key, screen] of screens) {
+      if (Date.now() - screen.updated > 1000) {
+        localStorage.removeItem(key);
+      }
+    }
+  }
+
+  function makeSVG() {
+    const screenPath = new PathMe();
+    // Set the SVG viewBox using the screen size
+    svgRef.current?.setAttribute(
+      "viewBox",
+      `0 0 ${window.screen.availWidth} ${window.screen.availHeight}`
+    );
+    svgRef.current?.setAttribute("width", `${window.screen.availWidth}px`);
+    svgRef.current?.setAttribute("height", `${window.screen.availHeight}px`);
+    // OFfset it by the window position
+    svgRef.current?.setAttribute(
+      "style",
+      `transform: translate(-${window.screenX}px, -${window.screenY}px)`
+    );
+    const screens = getScreens();
+    screens
+      .map(([key, screen]) => {
+        const x = screen.screenX + screen.width / 2;
+        const y = screen.screenY + screen.height / 2;
+        return [key, { ...screen, x, y }];
+      })
+      .forEach(([key, screen], i) => {
+        if (i === 0) {
+          screenPath.moveTo(screen.x, screen.y);
+        } else {
+          screenPath.lineTo(screen.x, screen.y);
+        }
+        // if (i === screens.length - 1) {
+        // screenPath.lineTo(screens[0][1].x, screens[0][1].y);
+        // }
+      });
+
+    screenPath.closePath();
+    pathRef?.setAttribute("d", screenPath.toString());
+  }
+
+  useEffect(() => {
+    setTimers([
+      setInterval(setScreenDetails, 10),
+      setInterval(displayStats, 10),
+      setInterval(removeOld, 100),
+      setInterval(makeSVG, 10),
+    ]);
+  }, []);
+
+  clear?.addEventListener("click", restart);
+  window.addEventListener("beforeunload", removeScreen);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="">
+      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
+        <path
+          fill="none"
+          stroke="#ffc600"
+          stroke-width="5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M271,74 L1015,344 M271,74 L271,74"
+        ></path>
+      </svg>
     </main>
-  )
+  );
 }
